50ccb4aab22ae8b8c6342da9dfe7bc01f6d36835
The-Mars-Guy <aclailhacar@gmail.com>
2026-03-19 17:36:19 -0400

    Delete src directory

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import OpentronsBuilder from './opentrons-builder.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <OpentronsBuilder />
-  </React.StrictMode>
-)
